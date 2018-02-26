095955875cd467914bb3330c1c3f279014c7b26c
Bret Gourdie <bretgourdie@gmail.com>
2018-02-26 00:29:48 -0500

    Added PokeApiRepository

---
+++ DataAccess/Repositories/PokeApi/PokeApiRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace DataAccess.Repositories.PokeApi
+{
+    public class PokeApiRepository
+    {
+        const string BaseUrl = "http://pokeapi.co/api/v2/";
+
+        public T Execute<T>(RestRequest request) where T : new()
+        {
+            var client = new RestClient();
+            client.BaseUrl = new System.Uri(BaseUrl);
+
+            var response = client.Execute<T>(request);
+
+            if (response.ErrorException != null)
+            {
+                const string message = "Error retrieving response. Check inner details for more information.";
+                var exception = new ApplicationException(message, response.ErrorException);
+                throw exception;
+            }
+
+            return response.Data;
+        }
+    }
+}
