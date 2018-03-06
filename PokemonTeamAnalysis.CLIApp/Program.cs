f3db2a59e35797d0b46dde716229562bab2e3c13
Bret Gourdie <bretgourdie@gmail.com>
2018-03-06 13:26:35 -0500

    Add CLI App for testing

---
+++ PokemonTeamAnalysis.CLIApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonTeamAnalysis.CLIApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            testProgram();
+        }
+
+        static void testProgram()
+        {
+            
+        }
+    }
+}
