d40d9c0120c3c9fe75afc71fc612dcb88e5dc543
Bret Gourdie <bretgourdie@gmail.com>
2018-03-24 16:42:58 -0400

    Begin processing Pokemon list

--- PokemonTeamAnalysis.CLIApp/Program.cs
+++ PokemonTeamAnalysis.CLIApp/Program.cs
@@ -13,12 +13,35 @@ namespace PokemonTeamAnalysis.CLIApp
         static void Main(string[] args)
         {
             var pokemonList = promptForPokemon();
+
+            if (isValidPokemonList(pokemonList))
+            {
+                processPokemonList(pokemonList);
+            }
+        }
+
+        static void processPokemonList(IList<Pokemon> pokemonList)
+        {
+            foreach (var pokemon in pokemonList)
+            {
+                foreach (var pokemonType in pokemon.Types)
+                {
+                    var singleType = pokemonType.Type.Content;
+
+
+                }
+            }
             // Pokemon
             // - PokemonType
             //   - SingleType
             //     - TypeRelations
             //       - SingleType
             //       - etc.
+        }
+
+        static bool isValidPokemonList(IList<Pokemon> pokemonList)
+        {
+            return pokemonList != null && pokemonList.Count > 0;
         }
 
         static IList<Pokemon> promptForPokemon()
